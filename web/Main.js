be04f14543f985ec82f12704f5cbe81c45240100
Stephen <steveprice971@hotmail.com>
2019-02-26 11:22:40 -0700

    main JS

---
+++ web/Main.js
@@ -0,0 +1,19 @@
+let box1 = {
+    id: "Item1",
+    color: "purple",
+    children: "This is box1"
+};
+let box2 = {
+    id: "Item 2",
+    color: "yellow",
+    children: "This is box2"
+};
+let box3 = {
+    id: "Item3",
+    color: "orange",
+    children: "This is box3"
+};
+
+let boxes = ["box1", "box2", "box3"]
+
+let myDivRef = document.getElementById(boxes[1].id)
