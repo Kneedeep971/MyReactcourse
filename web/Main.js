c12f3dde0744a3ba26ce4ae5341b4336304c2457
Stephen <steveprice971@hotmail.com>
2019-02-26 11:37:08 -0700

    Colors

--- web/Main.js
+++ web/Main.js
@@ -16,8 +16,14 @@ let box3 = {
 
 let boxes = [box1, box2, box3];
 
+let myDivRef = document.getElementById(boxes[0].id);
+myDivRef.innerHTML = boxes[0].children;
+myDivRef.style.backgroundColor = boxes[0].color;
+
+let myDivRef = document.getElementById(boxes[1].id);
+myDivRef.innerHTML = boxes[1].children;
+myDivRef.style.backgroundColor = boxes[1].color;
+
 let myDivRef = document.getElementById(boxes[2].id);
-
 myDivRef.innerHTML = boxes[2].children;
-
 myDivRef.style.backgroundColor = boxes[2].color;
