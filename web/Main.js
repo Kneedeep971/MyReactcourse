77d3a6ebafe8bc4c4b64177a158eab492654bec2
Stephen <steveprice971@hotmail.com>
2019-02-26 12:47:06 -0700

    For Loop

--- web/Main.js
+++ web/Main.js
@@ -16,14 +16,21 @@ let box3 = {
 
 let boxes = [box1, box2, box3];
 
-let myDivRef = document.getElementById(boxes[0].id);
-myDivRef.innerHTML = boxes[0].children;
-myDivRef.style.backgroundColor = boxes[0].color;
+//let myDivRef = document.getElementById(boxes[0].id);
+//myDivRef.innerHTML = boxes[0].children;
+//myDivRef.style.backgroundColor = boxes[0].color;
 
-myDivRef = document.getElementById(boxes[1].id);
-myDivRef.innerHTML = boxes[1].children;
-myDivRef.style.backgroundColor = boxes[1].color;
+//myDivRef = document.getElementById(boxes[1].id);
+//myDivRef.innerHTML = boxes[1].children;
+//myDivRef.style.backgroundColor = boxes[1].color;
 
-myDivRef = document.getElementById(boxes[2].id);
-myDivRef.innerHTML = boxes[2].children;
-myDivRef.style.backgroundColor = boxes[2].color;
+//myDivRef = document.getElementById(boxes[2].id);
+//myDivRef.innerHTML = boxes[2].children;
+//myDivRef.style.backgroundColor = boxes[2].color;
+
+for (let index = 0; index < 3; index++) {
+
+     let myDivRef = document.getElementById(boxes[index].id);
+     myDivRef.innerHTML = boxes[index].children;
+     myDivRef.style.backgroundColor = boxes[index].color;
+}
