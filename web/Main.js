df355f51359e0bfdeb67d505fba93cbabd83da62
Stephen <steveprice971@hotmail.com>
2019-02-26 12:26:01 -0700

    Finished

--- web/Main.js
+++ web/Main.js
@@ -20,3 +20,10 @@ let myDivRef = document.getElementById(boxes[0].id);
 myDivRef.innerHTML = boxes[0].children;
 myDivRef.style.backgroundColor = boxes[0].color;
 
+myDivRef = document.getElementById(boxes[1].id);
+myDivRef.innerHTML = boxes[1].children;
+myDivRef.style.backgroundColor = boxes[1].color;
+
+myDivRef = document.getElementById(boxes[2].id);
+myDivRef.innerHTML = boxes[2].children;
+myDivRef.style.backgroundColor = boxes[2].color;
